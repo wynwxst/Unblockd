2c076fb4322ecb6752abba0a7e82363bb63b05b9
Sakurai07 <cool5tarXV@gmail.com>
2021-03-18 10:16:35 +0800

    added unblockd lol

---
+++ setup.py
@@ -0,0 +1,7 @@
+import os
+os.system("npm install")
+os.system("cd unblockd")
+os.system("npm install")
+os.system("cd ..")
+os.system("echo starting...")
+os.system("python start.py")
---
+++ start.py
@@ -0,0 +1,4 @@
+import os
+path = os.system("pwd")
+print(path)
+os.system("npm start")
